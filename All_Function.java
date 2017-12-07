f3afc240a70b8b78f19f35b8314dfb9158a271a6
charlyhs24 <charlysitumorang24@gmail.com>
2017-12-08 05:11:26 +0700

    tupro

---
+++ All_Function.java
@@ -0,0 +1,6 @@
+package Simulated_Annealing;
+import java.util.Random;
+
+public class All_function{
+  
+}
---
+++ Simulated_Anealling.java
@@ -0,0 +1,12 @@
+package SimulatedAnnealing;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Simulated_Anealling{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("cobacoba :");
+    int i = sc.nextInt();
+    System.out.print("angka sebelum : "+i);
+  }
+}
